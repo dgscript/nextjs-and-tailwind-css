72d8a74465bd7f287c34f6acd7f677a721466813
rezird <148511997+rezird@users.noreply.github.com>
2025-07-09 17:18:11 -0300

    fixed skeleton animation

--- app/learning/page.jsx
+++ app/learning/page.jsx
@@ -2,11 +2,12 @@ import style from "./page.module.css";
 
 export default function Learning() {
   return (
-    <main className="flex justify-center items-center flex-col h-[80dvh]">
+    <main className="flex justify-center items-center flex-col h-[80dvh] relative overflow-hidden z-[-10]">
       <p className="text-3xl font-bold max-sm:text-[1rem]">
         Page under maintance!
       </p>
       <img src="/skeleton.gif" alt="Come back later!" className="my-10" />
+      {/* skeleton 2 will cross the page */}
       <img
         src="/skeleton2.gif"
         alt="Come back later!"
